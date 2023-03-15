3b551597148fe0ef2ceec56fe6291c0ded0b124e
goeldner <ch@goeldner.ch>
2023-03-15 22:57:32 +0100

    Bump scrscr version to 1.2.0

--- Formula/scrscr.rb
+++ Formula/scrscr.rb
@@ -1,8 +1,8 @@
 class Scrscr < Formula
   desc "A command line tool that decorates screenshots with nice backgrounds and captions."
   homepage "https://github.com/goeldner/swift-screenshot-scribbler"
-  url "https://github.com/goeldner/swift-screenshot-scribbler.git", tag: "1.1.0"
-  version "1.1.0"
+  url "https://github.com/goeldner/swift-screenshot-scribbler.git", tag: "1.2.0"
+  version "1.2.0"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
